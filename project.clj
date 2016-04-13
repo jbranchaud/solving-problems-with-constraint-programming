6e4d4f6edce53c534729d5a46a24b235c465a23c
jbranchaud <jbranchaud@gmail.com>
2016-04-13 08:02:25 -0500

    Add project manifest for lein's benefit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject solving-puzzles-with-constraint-programming "0.1.0-SNAPSHOT"
+  :description "Solving Puzzles with Constraint Programming"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [loco "0.3.1"]])
