a84adc0fe089e54d1d51a5f905a41bc3d7b52735
jbranchaud <jbranchaud@gmail.com>
2016-04-13 09:44:47 -0500

    Include the combinatorics library

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject solving-puzzles-with-constraint-programming "0.1.0-SNAPSHOT"
   :description "Solving Puzzles with Constraint Programming"
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.combinatorics "0.1.1"]
                  [loco "0.3.1"]])
